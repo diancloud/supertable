7da6913545635ae38b9af7437ced19a9776188e9
Weiping <weiping@diancloud.com>
2015-10-31 12:27:06 +0800

    Create New Init

---
+++ src/supertable.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * SuperTable 基类
+ *
+ * CLASS 
+ *
+ * 	   SuperTable 
+ *
+ * USEAGE: 
+ *
+ *     不要直接使用
+ * 
+ */
+
+namespace TuanDuiMao;
+
+/**
+ * SuperTable
+ */
+class SuperTable {
+	
+	function __construct() {
+	}
+	
+}
